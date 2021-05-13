370ffcbc6b5a6eeb41e67d6c558533612c11e681
mathgoncalves <matheusgoncalvesgraca@outlook.com>
2021-05-13 19:42:41 -0300

    Update keys and type key

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-class Publication(models.Model):
-    nome_da_empresa = models.CharField(max_length=200)
-    descrição_do_serviço = models.TextField()
-    CNPJ = models.TextField()
-    número_da_nota = models.CharField(max_length=256)
-    data_e_hora_da_emissão = models.CharField(max_length=200)
-    valor_da_nota = models.CharField(max_length=200)
-    publication_date = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class NotaFiscal(models.Model):
+    empresa = models.CharField(max_length=200)
+    descricao = models.TextField()
+    cnpj = models.TextField()
+    nota = models.CharField(max_length=256)
+    data_emissao = models.CharField(max_length=200)
+    valor_nota = models.FloatField()
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
